860e246cef0572be7fc34c511cb2fb06429f3dcc
Blane <Blanestask@gmail.com>
2019-05-09 21:18:54 -0400

    pushing 05 labs

--- labs/05_functions/05_01_tasks.py
+++ labs/05_functions/05_01_tasks.py
@@ -14,3 +14,21 @@ Write a script that completes the following tasks.
 # calls a function that determines whether the number is divisible by 4 or 7 exclusively
 
 # print the results
+
+number = input("Enter a value from 1 and 1,000,000,000: ")
+num = int(number)
+x = num % 4
+y = num % 7
+
+def div(num):
+
+    if x == 0 and y == 0:
+        print("This value is divisible by 4 and 7.")
+    elif x == 0:
+        print("This value is divisible by 4.")
+    elif y == 0:
+        print("This value is divisible by 7.")
+    else:
+        print("This value is not divisible by 4 or 7.")
+
+div(sum)
--- labs/05_functions/05_02_rps.py
+++ labs/05_functions/05_02_rps.py
@@ -2,36 +2,54 @@
 Code a game of rock paper scissors.
 
 '''
-
-
-# function to get hand based on number
-# the function should take in a number and return the string representation of the hand
 def get_hand(hand):
-    # 0 = scissor, 1 = rock, 2 = paper
+    scissors = 0
+    rock = 1
+    paper = 2
+    if hand == 0:
+        print("Your hand is scissors, good luck!")
+        return scissors
+    elif hand == 1:
+        print("Your hand is rock, good luck!")
+        return rock
+    else:
+        print("Your hand is paper, good luck!")
+        return paper
 
-    # for example if the variable hand is 0, it should return the string "scissor"
-    pass
+def get_comp(computer):
+    scissors = 0
+    rock = 1
+    paper = 2
+    if computer == 0:
+        print("The computer has scissors!")
+        return scissors
+    elif computer == 1:
+        print("The computer has rock!")
+        return rock
+    else:
+        print("The computer has paper!")
+        return paper
 
-# function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
+
 def determine_winner(computer, player):
-    pass
+    if computer == player:
+        print("You tied!")
+    elif computer == 2 and player != 0:
+        print("You lost :(")
+    elif computer == 1 and player != 2:
+        print("You lost :(")
+    elif computer == 0 and player != 1:
+        print("You lost :(")
+    else:
+        print("You won!")
 
+number = input("Give me a number from 0 to 2 to represent your hand. 0 = scissor, 1 = rock, 2 = paper: ")
+hand = int(number)
 
+import random
+computer = random.randint(0, 2)
 
-'''
-Start here
-'''
+get_hand(hand)
+get_comp(computer)
 
-# take in a number 0-2 from the user that represents their hand
-
-# generate a random number 0-2 to use for the computer's hand
-
-# call the function get_hand to get the string representation of the user's hand
-
-# call the function get_hand to get the string representation of the computer's hand
-
-# call the function determine_winner to figure out the winner
-
-# print out the player hand and computer hand
-
-# print out the winner
+determine_winner(computer,hand)
