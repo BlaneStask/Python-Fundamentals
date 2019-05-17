411aa0538218c430eb957f2f4e5e641ddde4edd9
Blane <Blanestask@gmail.com>
2019-05-17 11:35:45 -0400

    pushing 09 labs

--- labs/09_testing/09_01_unittest.py
+++ labs/09_testing/09_01_unittest.py
@@ -7,3 +7,23 @@ test should pass.
 Also include a test that does not pass.
 
 '''
+import unittest
+
+class TestNumberMethods(unittest.TestCase):
+
+    number = int(input("Enter a number: "))
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self, number):
+        self.number += self.number
+        print(number)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- labs/09_testing/09_02_tdd.py
+++ labs/09_testing/09_02_tdd.py
@@ -8,3 +8,23 @@ fail and write tests that will check and prevent failure.
 You do not need to implement the actual functions after writing the tests but you may.
 
 '''
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
