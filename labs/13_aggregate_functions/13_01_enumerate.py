01c0fe6b7d481652b08595968ad7fef690953b6b
Blane <Blanestask@gmail.com>
2019-05-18 11:57:33 -0400

    pushing 13 labs

--- labs/13_aggregate_functions/13_01_enumerate.py
+++ labs/13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,7 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+my_list = ["apple", "banana", "orange"]
+
+for index, obj1 in enumerate(my_list):
+    print(f"({index}, '{obj1}')", end="")
--- labs/13_aggregate_functions/13_02_sum.py
+++ labs/13_aggregate_functions/13_02_sum.py
@@ -2,3 +2,9 @@
 Write a simple aggregate function, sum(), that takes a list a returns the sum.
 
 '''
+input_string = input("Enter a list of values separated by a space: ")
+
+def sum_digits(digit):
+    return sum(int(x) for x in digit if x.isdigit())
+
+print("The sum of the values in the list are: ", sum_digits(input_string))
