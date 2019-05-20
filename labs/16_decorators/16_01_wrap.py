20e85f40c03e6a8456836b46348aff54c88fa8ae
Blane <Blanestask@gmail.com>
2019-05-20 11:24:29 -0400

    pushing 16 lab

--- labs/16_decorators/16_01_wrap.py
+++ labs/16_decorators/16_01_wrap.py
@@ -3,3 +3,18 @@ Write a decorator function that wraps text passed to it in a specified html tag.
 The user should be able to decide which tag to use.
 
 '''
+
+
+def decorator_func(initial_func):
+    def wrapper_func():
+        your_input = input("Type your own tag: ")
+        return initial_func()
+    return wrapper_func
+
+
+@decorator_func
+def dec():
+    print("What a good idea!")
+
+
+dec()
