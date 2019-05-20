b8c1dc3f95a3e3cd6b69b5a9ac1105c2e1847331
Blane <Blanestask@gmail.com>
2019-05-20 11:22:50 -0400

    pushing 15 labs

--- labs/15_variable_arguments/15_01_args.py
+++ labs/15_variable_arguments/15_01_args.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+
+def my_function(*args):
+    for value in args:
+        print(value)
+
+
+my_function(1, 2, 3, 4)
--- labs/15_variable_arguments/15_02_kwargs.py
+++ labs/15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+
+def my_function(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+
+
+my_function(Ben='hi', Bill='hello')
