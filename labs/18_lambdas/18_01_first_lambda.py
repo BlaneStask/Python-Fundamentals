da876c7d8888bfff50e7f3b0897d4d6685e39b3a
Blane <Blanestask@gmail.com>
2019-05-21 10:21:50 -0400

    pushing 18 labs

--- labs/18_lambdas/18_01_first_lambda.py
+++ labs/18_lambdas/18_01_first_lambda.py
@@ -2,3 +2,8 @@
 Write a lambda function that takes in 3 numbers and returns the sum of the numbers.
 
 '''
+numbers = input("Enter three numbers, separated my a space: ")
+
+num_sum = lambda x: sum(int(x) for x in numbers if x.isdigit())
+
+print("The sum of the values in the list are: ", num_sum(numbers))
--- labs/18_lambdas/18_02_return.py
+++ labs/18_lambdas/18_02_return.py
@@ -2,3 +2,6 @@
 Write a lambda function that does not take in an arguments but returns a value. Print the return value.
 
 '''
+value = lambda *args, **kwargs: None
+
+print("The return value is: ", value())
